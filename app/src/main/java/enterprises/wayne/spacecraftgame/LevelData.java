c95a13f41e20fec956b88e1867ce42026fa5fca0
aaronistheman <aarons.7007@gmail.com>
2016-02-07 20:45:50 -0800

    Fixed some mistakes with LevelData class

--- app/src/main/java/enterprises/wayne/spacecraftgame/LevelData.java
+++ app/src/main/java/enterprises/wayne/spacecraftgame/LevelData.java
@@ -1,19 +1,26 @@
 package enterprises.wayne.spacecraftgame;
 
-import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by Aaron on 2/7/2016.
  */
 public class LevelData {
-    public ArrayList<EnemyEntity> enemyEntities;
-    public float totalDistance;
+    public CopyOnWriteArrayList<EnemyEntityData> enemyData;
+    public float goalDistance;
     public int backgroundMusicResId;
 
-    public LevelData(ArrayList<EnemyEntity> enemyEntities,
-                     float totalDistance, int backgroundMusicResId) {
-        this.enemyEntities = enemyEntities;
-        this.totalDistance = totalDistance;
+    // Default constructor
+    public LevelData() {
+        this.enemyData = null;
+        this.goalDistance = 0;
+        this.backgroundMusicResId = -1; // should cause bug if not changed
+    }
+
+    public LevelData(CopyOnWriteArrayList<EnemyEntityData> enemyData,
+                     float goalDistance, int backgroundMusicResId) {
+        this.enemyData = enemyData;
+        this.goalDistance = goalDistance;
         this.backgroundMusicResId = backgroundMusicResId;
     }
 }
