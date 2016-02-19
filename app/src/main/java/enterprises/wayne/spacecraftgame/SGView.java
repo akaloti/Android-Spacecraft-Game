cbd9b34895bd9b904410d0ee18f5d86c57c56b27
aaronistheman <aarons.7007@gmail.com>
2016-02-18 17:26:02 -0800

    Background music only restarts if changed

--- app/src/main/java/enterprises/wayne/spacecraftgame/SGView.java
+++ app/src/main/java/enterprises/wayne/spacecraftgame/SGView.java
@@ -65,6 +65,7 @@ public class SGView extends SurfaceView
     private int mLossSound = -1;
 
     private MediaPlayer mBackgroundMusic;
+    private int mCurrentMusic = -1;
 
     private ArrayList<LevelData> mLevels;
     private LevelData mCurrentLevel;
@@ -147,6 +148,26 @@ public class SGView extends SurfaceView
                 new EnemyEntityData(Entity.Type.HUNTER_1, 0, 1000));
         levelData.enemyData.add(
                 new EnemyEntityData(Entity.Type.HUNTER_1, 0, 1000));
+        levelData.backgroundMusicResId = R.raw.king_galaxian;
+        mLevels.add(levelData);
+
+        // Level 3
+        levelData = new LevelData();
+        levelData.goalDistance = 200;
+        levelData.enemyData.add(
+                new EnemyEntityData(Entity.Type.SMALL_ASTEROID, 0, 1000));
+        levelData.backgroundMusicResId = R.raw.saber_wing;
+        mLevels.add(levelData);
+
+        // Level 4
+        levelData = new LevelData();
+        levelData.goalDistance = 400;
+        levelData.enemyData.add(
+                new EnemyEntityData(Entity.Type.HUNTER_1, 0, 1000));
+        levelData.enemyData.add(
+                new EnemyEntityData(Entity.Type.HUNTER_1, 0, 1000));
+        levelData.enemyData.add(
+                new EnemyEntityData(Entity.Type.HUNTER_1, 200, 1000));
         levelData.backgroundMusicResId = R.raw.saber_wing;
         mLevels.add(levelData);
     }
@@ -214,18 +235,21 @@ public class SGView extends SurfaceView
      * old music was stopped; new one was started
      */
     private void changeBackgroundMusic() {
-        if (mBackgroundMusic != null) {
-            // Remove old background music
-            mBackgroundMusic.stop();
-            mBackgroundMusic.release();
-        }
+        // Only restart the background music if there really is a change
+        if (mCurrentMusic != mCurrentLevel.backgroundMusicResId) {
+            if (mBackgroundMusic != null) {
+                // Remove old background music
+                mBackgroundMusic.stop();
+                mBackgroundMusic.release();
+            }
 
-        // Start new background music
-        mBackgroundMusic = MediaPlayer.create(mContext,
-                mCurrentLevel.backgroundMusicResId);
-        mBackgroundMusic.setLooping(true);
-        mBackgroundMusic.setVolume(1.0f, 1.0f);
-        mBackgroundMusic.start();
+            // Start new background music
+            mCurrentMusic = mCurrentLevel.backgroundMusicResId;
+            mBackgroundMusic = MediaPlayer.create(mContext, mCurrentMusic);
+            mBackgroundMusic.setLooping(true);
+            mBackgroundMusic.setVolume(1.0f, 1.0f);
+            mBackgroundMusic.start();
+        }
     }
 
     /**
